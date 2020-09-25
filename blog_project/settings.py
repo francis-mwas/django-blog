3fbc556428158f2edfe2763a1b036f3de3ef362c
francis-mwas <mwangif184@gmail.com>
2020-09-26 01:39:46 +0300

    Django blog application

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "%%-4i3t-c$b(+o-zy%kc_x!!hl@hoamu&t9e!=)bha-vl*jaqo"
+SECRET_KEY = "your-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
